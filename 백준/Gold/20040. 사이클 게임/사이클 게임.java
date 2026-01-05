3b05b88315a271b1be117a5a9c42bb62f9aa949a
minseo6753 <109271028+minseo6753@users.noreply.github.com>
2026-01-05 22:53:40 +0900

    [Gold IV] Title: 사이클 게임, Time: 888 ms, Memory: 247124 KB -BaekjoonHub

--- 백준/Gold/20040. 사이클 게임/사이클 게임.java
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.java
@@ -5,7 +5,7 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    private static final int INF = 1000001;
+    private static int[] parent;
 
     public static void main(String[] args) throws IOException {
 
@@ -13,7 +13,10 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int n = Integer.parseInt(st.nextToken());
-        Graph graph = new Graph(n);
+        parent = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
 
         int m = Integer.parseInt(st.nextToken());
         int cycle = 0;
@@ -21,39 +24,28 @@ public class Main {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            if (!graph.union(a, b) && cycle == 0) {
+            if (!union(a, b) && cycle == 0) {
                 cycle = i + 1;
             }
         }
         System.out.println(cycle);
     }
 
-    static class Graph {
-
-        private int[] parent;
-
-        Graph(int n) {
-            parent = new int[n];
-            for (int i = 0; i < n; i++) {
-                parent[i] = i;
-            }
+    private static int findRoot(int v) {
+        if (parent[v] == v) {
+            return v;
         }
-
-        private int findRoot(int v) {
-            if (parent[v] == v) {
-                return v;
-            }
-            return parent[v] = findRoot(parent[v]);
-        }
-
-        public boolean union(int v, int w) {
-            int root1 = findRoot(v);
-            int root2 = findRoot(w);
-            if (root1 != root2) {
-                parent[root1] = root2;
-                return true;
-            }
-            return false;
-        }
+        return parent[v] = findRoot(parent[v]);
     }
+
+    private static boolean union(int v, int w) {
+        int root1 = findRoot(v);
+        int root2 = findRoot(w);
+        if (root1 != root2) {
+            parent[root1] = root2;
+            return true;
+        }
+        return false;
+    }
+
 }
