6ce2f33c73d5dbaee10ce40cfaebf7da58cdcbb6
minseo6753 <109271028+minseo6753@users.noreply.github.com>
2026-01-09 23:22:05 +0900

    [Gold III] Title: 두 배열의 합, Time: 368 ms, Memory: 71872 KB -BaekjoonHub

--- 백준/Gold/2143. 두 배열의 합/두 배열의 합.java
+++ 백준/Gold/2143. 두 배열의 합/두 배열의 합.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -13,65 +13,48 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        long t = Integer.parseInt(br.readLine());
+        int t = Integer.parseInt(br.readLine());
 
         int n = Integer.parseInt(br.readLine());
-        long[] a = new long[n + 1];
-        a[0] = 0;
+        int[] a = new int[n];
         st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= n; i++) {
-            a[i] = a[i - 1] + Integer.parseInt(st.nextToken());
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
         }
 
         int m = Integer.parseInt(br.readLine());
-        long[] b = new long[m + 1];
-        b[0] = 0;
+        int[] b = new int[m];
         st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= m; i++) {
-            b[i] = b[i - 1] + Integer.parseInt(st.nextToken());
+        for (int i = 0; i < m; i++) {
+            b[i] = Integer.parseInt(st.nextToken());
         }
 
-        List<Long> prefixSumA = new ArrayList<>();
-        for (int i = 1; i <= n; i++) {
-            for (int j = i; j <= n; j++) {
-                prefixSumA.add(a[j] - a[i - 1]);
-            }
+        List<Integer> accSumA = accumulate(a);
+        List<Integer> accSumB = accumulate(b);
+
+        HashMap<Integer, Integer> mapA = new HashMap<>();
+        for (Integer accSum : accSumA) {
+            mapA.put(accSum, mapA.getOrDefault(accSum, 0) + 1);
         }
-        prefixSumA.sort(Comparator.naturalOrder());
-        List<Long> prefixSumB = new ArrayList<>();
-        for (int i = 1; i <= m; i++) {
-            for (int j = i; j <= m; j++) {
-                prefixSumB.add(b[j] - b[i - 1]);
-            }
-        }
-        prefixSumB.sort(Comparator.reverseOrder());
 
         long count = 0;
-        int pA = 0;
-        int pB = 0;
-        while (pA < prefixSumA.size() && pB < prefixSumB.size()) {
-            if (prefixSumA.get(pA) + prefixSumB.get(pB) == t) {
-                pA++;
-                pB++;
-                long sameA = 1;
-                long sameB = 1;
-                while (pA < prefixSumA.size() && prefixSumA.get(pA - 1).equals(prefixSumA.get(pA))) {
-                    sameA++;
-                    pA++;
-                }
-                while (pB < prefixSumB.size() && prefixSumB.get(pB - 1).equals(prefixSumB.get(pB))) {
-                    sameB++;
-                    pB++;
-                }
-                count += sameA * sameB;
-            } else if (prefixSumA.get(pA) + prefixSumB.get(pB) < t) {
-                pA++;
-            } else {
-                pB++;
-            }
+        for (Integer accSum : accSumB) {
+            count += mapA.getOrDefault(t - accSum, 0);
         }
 
         System.out.println(count);
+    }
+
+    private static List<Integer> accumulate(int[] arr) {
+        List<Integer> accSum = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            int sum = 0;
+            for (int j = i; j < arr.length; j++) {
+                sum += arr[j];
+                accSum.add(sum);
+            }
+        }
+        return accSum;
     }
 
 }
