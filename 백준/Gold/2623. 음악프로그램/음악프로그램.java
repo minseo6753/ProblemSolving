ebad6ad163689d9d433311260b7f0557fadfaadd
minseo6753 <109271028+minseo6753@users.noreply.github.com>
2026-01-14 00:44:42 +0900

    [Gold III] Title: 음악프로그램, Time: 116 ms, Memory: 14648 KB -BaekjoonHub

---
+++ 백준/Gold/2623. 음악프로그램/음악프로그램.java
@@ -0,0 +1,64 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        List<Integer>[] adj = new List[n + 1];
+        for (int i = 1; i <= n; i++) {
+            adj[i] = new ArrayList<>();
+        }
+        int[] inDegree = new int[n + 1];
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int number = Integer.parseInt(st.nextToken());
+            int prev = Integer.parseInt(st.nextToken());
+            for (int j = 1; j < number; j++) {
+                int singer = Integer.parseInt(st.nextToken());
+                adj[prev].add(singer);
+                inDegree[singer]++;
+                prev = singer;
+            }
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 1; i <= n; i++) {
+            if (inDegree[i] == 0) {
+                q.add(i);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            if(q.isEmpty()) {
+                System.out.println(0);
+                return;
+            }
+            Integer poll = q.poll();
+            sb.append(poll).append("\n");
+            for (Integer next : adj[poll]) {
+                inDegree[next]--;
+                if (inDegree[next] == 0) {
+                    q.add(next);
+                }
+            }
+        }
+        System.out.println(sb);
+    }
+
+}
