4a40f053088512104cde67ee42488307bcd98645
minseo6753 <109271028+minseo6753@users.noreply.github.com>
2026-01-18 22:18:38 +0900

    [Gold III] Title: 피리 부는 사나이, Time: 376 ms, Memory: 26396 KB -BaekjoonHub

---
+++ 백준/Gold/16724. 피리 부는 사나이/피리 부는 사나이.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static char[][] map;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        map = new char[n][m];
+
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < m; j++) {
+                map[i][j] = s.charAt(j);
+            }
+        }
+
+        visited = new boolean[n][m];
+        completed = new boolean[n][m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                recur(i,j);
+            }
+        }
+
+        System.out.println(count);
+    }
+
+    static boolean[][] visited;
+    static boolean[][] completed;
+    static int count;
+
+    static void recur(int r, int c) {
+        if (completed[r][c]) {
+            return;
+        }
+
+        if (visited[r][c]) {
+            count++;
+        } else {
+            visited[r][c] = true;
+            char direction = map[r][c];
+            if (direction == 'U') {
+                recur(r - 1, c);
+            } else if (direction == 'D') {
+                recur(r + 1, c);
+            } else if (direction == 'L') {
+                recur(r, c - 1);
+            } else if (direction == 'R') {
+                recur(r, c + 1);
+            }
+        }
+
+        completed[r][c]=true;
+    }
+
+}
+
