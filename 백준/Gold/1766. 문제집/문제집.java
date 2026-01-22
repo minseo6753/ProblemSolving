17b60d0cd50fb7b6c6b6225f1cbe36a52658d9e1
minseo6753 <109271028+minseo6753@users.noreply.github.com>
2026-01-22 20:10:24 +0900

    [Gold II] Title: 문제집, Time: 460 ms, Memory: 46244 KB -BaekjoonHub

---
+++ 백준/Gold/1766. 문제집/문제집.java
@@ -0,0 +1,56 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        List<Integer>[] graph = new List[n+1];
+        for (int i = 1; i <= n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        int[] indegree = new int[n + 1];
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            graph[a].add(b);
+            indegree[b]++;
+        }
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        for (int i = 1; i <= n; i++) {
+            if (indegree[i] == 0) {
+                queue.add(i);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        while (!queue.isEmpty()) {
+            Integer poll = queue.poll();
+            sb.append(poll).append(" ");
+            for (Integer next : graph[poll]) {
+                indegree[next]--;
+                if (indegree[next] == 0) {
+                    queue.add(next);
+                }
+            }
+        }
+
+        System.out.println(sb);
+    }
+
+}
