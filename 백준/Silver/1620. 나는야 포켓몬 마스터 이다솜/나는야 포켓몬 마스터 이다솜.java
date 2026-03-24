28458fdc0ff733d8de73bf9d3ec863e10525839b
minseo6753 <109271028+minseo6753@users.noreply.github.com>
2026-03-25 00:33:46 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 728 ms, Memory: 142924 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -1,10 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -16,14 +13,12 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        List<String> names = new ArrayList<>();
+        String[] names = new String[n + 1];
         HashMap<String, Integer> map = new HashMap<>();
-
-        names.add("");
 
         for (int i = 1; i <= n; i++) {
             String name = br.readLine();
-            names.add(name);
+            names[i] = name;
             map.put(name, i);
         }
 
@@ -32,7 +27,7 @@ public class Main {
         for (int i = 0; i < m; i++) {
             String question = br.readLine();
             if (question.matches("[0-9]+")) {
-                sb.append(names.get(Integer.parseInt(question)));
+                sb.append(names[Integer.parseInt(question)]);
             }
             else {
                 sb.append(map.get(question));
